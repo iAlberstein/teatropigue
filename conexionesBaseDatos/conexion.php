ef27ea366187dd87864ca2c999c8c9c55131dca3
Bruno Alberstein <albersteinbruno@gmail.com>
2023-10-28 15:37:43 -0300

    prueba

--- conexionesBaseDatos/conexion.php
+++ conexionesBaseDatos/conexion.php
@@ -1,8 +1,8 @@
 <?php
 
     $dbhost = 'localhost';
-    $dbuser = 'c1382483_teatro';
-    $dbpass = 'mePU03vugo';
+    $dbuser = 'c1382483';
+    $dbpass = 'deNU24viwo';
     $dbname = 'c1382483_teatro';
     
     $conectar = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);

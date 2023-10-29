8f2a66fa92f4ce56acdfc4cb8729413f8a11effd
Bruno Alberstein <albersteinbruno@gmail.com>
2023-10-29 14:41:51 -0300

    sqli

--- conexion.php
+++ conexion.php
@@ -1,5 +1,9 @@
 <?php
 
-	$conex = mysql_connect ("localhost", "c1382483_teatro", "mePU03vugo", "c1382483_teatro")
+	$conex = new mysqli("localhost", "c1382483_teatro", "mePU03vugo", "c1382483_teatro")
+
+	if ($conex>connect_error) {
+		die("Error de conexión: " . $conex->connect_error);
+	}
 
 ?>

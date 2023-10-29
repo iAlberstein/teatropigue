25f6c2db3cd4ff4efb06198d7144a9951852117a
Bruno Alberstein <albersteinbruno@gmail.com>
2023-10-29 14:28:11 -0300

    modificar conexion.php

--- conexion.php
+++ conexion.php
@@ -1,10 +1,5 @@
 <?php
 
-	$dbhost = 'localhost';
-	$dbuser = 'c1382483_teatro';
-	$dbpass = 'mePU03vugo';
-	$dbname = 'c1382483_teatro';
+	$conex = mysql_connect ("localhost", "c1382483_teatro", "mePU03vugo", "c1382483_teatro")
 
-	$conex = mysql_connect($dbhost, $dbuser, $dbpass) or die ('Ocurrio un error al conectarse al servidor mysql');
-	mysql_select_db($dbname);
 ?>

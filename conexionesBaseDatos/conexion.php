30cb04d6cfc3232459b809dcd32bdcfe56fe942c
Bruno Alberstein <albersteinbruno@gmail.com>
2023-10-29 13:42:40 -0300

    prueba 2

--- conexionesBaseDatos/conexion.php
+++ conexionesBaseDatos/conexion.php
@@ -3,7 +3,7 @@
 	$dbhost = 'localhost';
 	$dbuser = 'c1382483_teatro';
 	$dbpass = 'mePU03vugo';
-	$dbname = 'c1382483_teatro';
+	$dbname = 'baseDatos';
 
 	$conn = mysql_connect($dbhost, $dbuser, $dbpass) or die ('Ocurrio un error al conectarse al servidor mysql');
 	mysql_select_db($dbname);

eedfe8c80130ea0dfbfe2996a28e41bd11b6e5ff
Bruno Alberstein <albersteinbruno@gmail.com>
2023-10-29 14:17:01 -0300

    se cambió origen de los php

--- guardar.php
+++ guardar.php
@@ -3,9 +3,10 @@
     
 
     if(isset($_POST['register'])){
-        if (strlen($_POST['name']) < 1 && strlen($_POST['email']) < 1) {
+        if (strlen($_POST['name']) >= 1 && strlen($_POST['email']) >= 1) {
             $nombre  = trim($_POST['name']);
             $email = trim($_POST['email']);
+            $fechareg = date ("d/m/y")
             $consulta = "INSERT INTO baseDatos(nombre, email, fecha_reg) VALUES ('$name','$email','$fechareg')";
             $resultado = mysqli_query($conex, $consulta);
             if ($resultado) {

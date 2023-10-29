bf1f12034456feceff95b40a4e634b21d8809bb8
Bruno Alberstein <albersteinbruno@gmail.com>
2023-10-29 14:22:17 -0300

    punto y coma

--- guardar.php
+++ guardar.php
@@ -1,12 +1,13 @@
 <?php
-    include "./conexion.php";
+
+    include "conexion.php";
     
 
     if(isset($_POST['register'])){
         if (strlen($_POST['name']) >= 1 && strlen($_POST['email']) >= 1) {
             $nombre  = trim($_POST['name']);
             $email = trim($_POST['email']);
-            $fechareg = date ("d/m/y")
+            $fechareg = date ("d/m/y");
             $consulta = "INSERT INTO baseDatos(nombre, email, fecha_reg) VALUES ('$name','$email','$fechareg')";
             $resultado = mysqli_query($conex, $consulta);
             if ($resultado) {

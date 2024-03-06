7e563554bb7bbb6a6f7ee835006996bca644bb56
Bruno Alberstein <albersteinbruno@gmail.com>
2024-03-06 17:03:32 -0300

    algo

---
+++ components/landing/landing.js
@@ -0,0 +1,14 @@
+import './landing.css'
+
+const landing => () {
+    return (
+        <div class="cuerpo">
+            <p class="titulo">SITIO WEB EN CONSTRUCCIÓN</p>
+            <figure>
+                <img src="../images/LOGO.TEATRO.NEGRO.C.FONDO.BLANCO.JPG" alt="LOGO TEATRO">
+            </figure>
+        </div>
+    )
+}
+
+export default landing
